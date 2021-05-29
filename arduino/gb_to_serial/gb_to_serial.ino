bc178aff79c9f9f7f8a14a57cd9b1daa8080bc1d
Matt Penny <mwpenny@users.noreply.github.com>
2021-05-29 15:40:52 -0400

    Arduino-based Game Boy serial interface
    The Arduino will mimic a Game Boy operating in master mode (driving the
    clock line). Each byte written over serial will be forwarded to the
    connected Game Boy, and the received byte will be written back to
    the serial connection.

---
+++ arduino/gb_to_serial/gb_to_serial.ino
@@ -0,0 +1,52 @@
+const int BAUD = 28800;  // Fastest that I found to be stable
+const int PIN_CLK = 2;
+const int PIN_SO = 3;
+const int PIN_SI = 4;
+
+void setup()
+{
+    Serial.begin(BAUD, SERIAL_8N1);
+    pinMode(PIN_CLK, OUTPUT);
+    pinMode(PIN_SO, OUTPUT);
+    pinMode(PIN_SI, INPUT_PULLUP);
+
+    // Signal to the PC that we are ready
+    Serial.write(0);
+}
+
+byte transfer_byte(byte tx)
+{
+    byte rx = 0;
+
+    for (int i = 0; i < 8; ++i)
+    {
+        digitalWrite(PIN_SO, (tx & 0x80) ? HIGH : LOW);
+        tx <<= 1;
+
+        // http://www.devrs.com/gb/files/gblpof.gif
+        // 120 us/bit
+        digitalWrite(PIN_CLK, LOW);
+        delayMicroseconds(60);
+
+        byte rx_bit = (digitalRead(PIN_SI) == HIGH) ? 1 : 0;
+        rx = (rx << 1) | rx_bit;
+
+        digitalWrite(PIN_CLK, HIGH);
+        delayMicroseconds(60);
+    }
+
+    return rx;
+}
+
+void loop()
+{
+    while (!Serial.available())
+    {
+        delay(100);
+    }
+
+    byte tx = Serial.read();
+    byte rx = transfer_byte(tx);
+
+    Serial.write(rx);
+}
